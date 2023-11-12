1a7da7cf285f35e2579aa5e9bdb1b826f2cdd020
kripalpatel16 <137495437+kripalpatel16@users.noreply.github.com>
2023-11-12 12:06:34 +0530

    Add files via upload

---
+++ module-5/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'SplashScreen.dart';
+
+
+
+void main()
+{
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
